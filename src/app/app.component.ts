a66fe9520c06cb0515115137ae3031414e7335d6
unifiedsaad <saad.m@whyunified.com>
2020-02-07 21:16:45 +0500

    fom angular and add post http request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { NgbRatingConfig } from "@ng-bootstrap/ng-bootstrap";
+import { HttpClient } from "@angular/common/http";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  providers: [NgbRatingConfig]
 })
 export class AppComponent {
-  title = 'AngularForm';
+  title = "AngularForm";
+  currentRate = 0;
+  AngForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    config: NgbRatingConfig
+  ) {
+    // customize default values of rating
+    config.max = 5;
+    this.createContactForm();
+  }
+  createContactForm() {
+    this.AngForm = this.formBuilder.group({
+      content: [""],
+      url: [""],
+      rating: [this.currentRate]
+    });
+  }
+
+  onSubmit() {
+    this.AngForm.value.rating = this.currentRate;
+    this.http.post("urlHere", this.AngForm.value).subscribe(responseData => {
+      console.log(responseData);
+      //redirect or whatever
+    });
+    console.log("Your form data : ", this.AngForm.value);
+  }
 }
